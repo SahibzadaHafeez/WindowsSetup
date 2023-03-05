596fea3ab085b5fde291452f915e695e80cfc647
SahibzadaHafeez <42448306+SahibzadaHafeez@users.noreply.github.com>
2023-03-05 13:18:45 +0500

    Added Laptop Essentials

---
+++ Laptop_Essentials.ps1
@@ -0,0 +1,7 @@
+Rename-Computer -NewName "Shitty-Laptop"
+winget install Google.Chrome
+winget install Nilesoft.Shell
+winget install Discord.Discord
+winget install 7zip.7zip
+winget install Microsoft.VisualStudioCode
+winget install PrivateInternetAccess.PrivateInternetAccess
---
+++ all_apps.ps1
@@ -0,0 +1,25 @@
+Rename-Computer -NewName "Shitty-PC"
+winget install Google.Chrome
+winget install Nilesoft.Shell
+winget install Discord.Discord
+winget install Wagnardsoft.DisplayDriverUninstaller
+winget install TechPowerUp.NVCleanstall
+winget install 7zip.7zip
+winget install Microsoft.VisualStudioCode
+winget install PrivateInternetAccess.PrivateInternetAccess
+
+winget install VideoLAN.VLC
+winget install Git.Git
+winget install GitHub.GitHubDesktop
+winget install Obsidian.Obsidian
+winget install OBSProject.OBSStudio
+winget install Microsoft.PowerToys
+winget install qBittorrent.qBittorrent
+winget install ShareX.ShareX
+winget install Valve.Steam
+winget install Telegram.TelegramDesktop
+winget install WhatsApp.WhatsApp
+winget install Unity.UnityHub
+winget install KDE.Kdenlive
+winget install XPDP32QDDFL4PK #Clip Studio
+winget install TheDocumentFoundation.LibreOffice
