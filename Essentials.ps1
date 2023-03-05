14427080de554ba39084f7edc8f91ca8ff625bbf
SahibzadaHafeez <42448306+SahibzadaHafeez@users.noreply.github.com>
2023-03-05 18:55:17 +0500

    Updated app list

--- Essentials.ps1
+++ Essentials.ps1
@@ -1,6 +1,8 @@
-Rename-Computer -NewName "Shitty-Laptop"
+Rename-Computer -NewName "Shitty-PC"
 winget install Google.Chrome
 winget install Nilesoft.Shell
 winget install Discord.Discord
+winget install Wagnardsoft.DisplayDriverUninstaller
+winget install TechPowerUp.NVCleanstall
 winget install 7zip.7zip
 winget install PrivateInternetAccess.PrivateInternetAccess --source winget
---
+++ Get_All.ps1
@@ -0,0 +1,18 @@
+winget install Microsoft.VisualStudioCode
+winget install VideoLAN.VLC
+winget install Git.Git
+winget install GitHub.GitHubDesktop
+winget install Obsidian.Obsidian
+winget install OBSProject.OBSStudio
+winget install Microsoft.PowerToys
+winget install qBittorrent.qBittorrent
+winget install ShareX.ShareX
+winget install Valve.Steam
+winget install Telegram.TelegramDesktop
+winget install WhatsApp.WhatsApp
+winget install Unity.UnityHub
+winget install KDE.Kdenlive
+winget install XPDP32QDDFL4PK #Clip Studio
+winget install TheDocumentFoundation.LibreOffice
+winget install Microsoft.VisualStudio.2022.Community
+wsl --install -d Ubuntu
